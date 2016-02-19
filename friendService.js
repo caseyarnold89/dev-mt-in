3da8d5dc07b14af6258403cbe07bb3b653bba732
Casey Arnold <caseyarnold89@gmail.com>
2016-02-18 19:21:01 -0700

    Create friendService

---
+++ friendService.js
@@ -0,0 +1,9 @@
+var app = angular.module('devMtIn')
+
+app.service('friendService',function($http) {
+    var baseUrl = 'http://connections.devmount.ain/';
+    
+    this.findFriends = function(userId, query) {
+        
+    }
+});
